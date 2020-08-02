aeec2bb82a7896a22d152ccda09f6acfc30baca5
LukeHallett <luke@larval.co.uk>
2020-08-02 22:04:17 +0100

    Fix register with new api Check date of birth

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,42 +1,99 @@
 <template>
-    <section class="section">
-        <div class="columns is-centered">
-            <div class="column is-4">
-                <h2 class="title is-2">Register</h2>
-                <form method="post" @submit.prevent="registerUser">
-                    <div class="field">
-                        <label class="label">Username</label>
-                        <div class="control">
-                            <input class="input" type="text" v-model="register.username" autocomplete="username">
+    <div class="ui main container">
+        <div class="ui basic segment color white">
+            <div class="ui middle aligned two column centered grid">
+                <div class="column">
+                    <br><br><br><br><br>
+                    <h2 style="text-align: center">Register to Voxtl</h2>
+                    <form class="ui form" method="post" @submit.prevent="registerUser">
+                        <div class="field">
+                            <label class="color white">Username</label>
+                            <input required type="text" v-model="register.username" autocomplete="username">
                         </div>
-                    </div>
-                    <div class="field">
-                        <label class="label">Email</label>
-                        <div class="control">
-                            <input class="input" type="email" v-model="register.email" autocomplete="email">
+
+                        <div class="field">
+                            <label class="color white">Email</label>
+                            <input required type="email" v-model="register.email" autocomplete="email">
                         </div>
-                    </div>
-                    <div class="field">
-                        <label class="label">Password</label>
-                        <div class="control">
-                            <input class="input" type="password" v-model="register.password" autocomplete="new-password">
+
+                        <div class="field">
+                            <label class="color white">Password</label>
+                            <input required type="password" v-model="register.password" autocomplete="new-password">
                         </div>
-                    </div>
-                    <div class="field">
-                        <label class="label">Confirm Password</label>
-                        <div class="control">
-                            <input class="input" type="password" v-model="register.confirm" autocomplete="new-password">
+
+                        <div class="field">
+                            <label class="color white">Confirm Password</label>
+                            <input required type="password" v-model="register.confirm" autocomplete="new-password">
                         </div>
-                    </div>
-                    <div class="field">
-                        <div class="control">
-                            <button class="button is-primary">Register</button>
+
+                        <div class="three fields">
+                            <div class="six wide field">
+                                <label class="color white">Date of Birth</label>
+                                <select required class="ui field search dropdown" v-model="register.birthday">
+                                    <option value="">Day</option>
+                                    <option value="01">1</option>
+                                    <option value="02">2</option>
+                                    <option value="03">3</option>
+                                    <option value="04">4</option>
+                                    <option value="05">5</option>
+                                    <option value="06">6</option>
+                                    <option value="07">7</option>
+                                    <option value="08">8</option>
+                                    <option value="09">9</option>
+                                    <option value="10">10</option>
+                                    <option value="11">11</option>
+                                    <option value="12">12</option>
+                                    <option value="13">13</option>
+                                    <option value="14">14</option>
+                                    <option value="15">15</option>
+                                    <option value="16">16</option>
+                                    <option value="17">17</option>
+                                    <option value="18">18</option>
+                                    <option value="19">19</option>
+                                    <option value="20">20</option>
+                                    <option value="21">21</option>
+                                    <option value="22">22</option>
+                                    <option value="23">23</option>
+                                    <option value="24">24</option>
+                                    <option value="25">25</option>
+                                    <option value="26">26</option>
+                                    <option value="27">27</option>
+                                    <option value="28">28</option>
+                                    <option value="29">29</option>
+                                    <option value="30">30</option>
+                                    <option value="30">31</option>
+                                </select>
+                            </div>
+                            <div class="six wide field">
+                                <label>‎‏‏‎ ‎‎</label>
+                                <select required class="ui fluid search dropdown" v-model="register.birthmonth">
+                                    <option value="">Month</option>
+                                    <option value="01">January</option>
+                                    <option value="02">February</option>
+                                    <option value="03">March</option>
+                                    <option value="04">April</option>
+                                    <option value="05">May</option>
+                                    <option value="06">June</option>
+                                    <option value="07">July</option>
+                                    <option value="08">August</option>
+                                    <option value="09">September</option>
+                                    <option value="10">October</option>
+                                    <option value="11">November</option>
+                                    <option value="12">December</option>
+                                </select>
+                            </div>
+                            <div class="six wide field">
+                                <label>‏‏‎ ‎</label>
+                                <input required type="text" placeholder="Year" v-model="register.birthyear">
+                            </div>
                         </div>
-                    </div>
-                </form>
+
+                        <button class="ui button">Register</button>
+                    </form>
+                </div>
             </div>
         </div>
-    </section>
+    </div>
 </template>
 
 <script>
@@ -53,14 +110,18 @@
                     username: '',
                     email: '',
                     password: '',
-                    confirm: ''
+                    confirm: '',
+                    birthday: '',
+                    birthmonth: '',
+                    birthyear: '',
                 }
             }
         },
         methods: {
             async registerUser() {
-                await this.$axios.post('http://51.15.105.208:3030/v1/internal/auth/register', this.register).then()
-                    .catch(error => {
+                await this.$axios.post('https://api.voxtl.tv/v1/internal/auth/register', this.register).then(
+                    this.$router.push('/auth/login')
+                ).catch(error => {
                         console.log(error);
                     });
             }
