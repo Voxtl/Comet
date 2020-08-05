d66978470e384ed5718a93f7625a722d4e8a8b7e
LukeHallett <luke@larval.co.uk>
2020-08-05 11:29:27 +0100

    Fix auth redirects

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,7 @@ export default {
     },
     auth: {
         redirect: {
-            login: '/login',
+            login: '/auth/login',
             home: '/'
         },
         strategies: {
