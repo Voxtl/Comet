1423f540e2b1d4a321028334e3684fe46b69c3c7
LukeHallett <luke@larval.co.uk>
2020-08-05 11:29:04 +0100

    Add list of issues

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,10 @@
         <div class="ui card">
             <div class="content">
                 Welcome to Voxtl 0.2
+                <br><br>
+                Known issues:<br>
+                - Dropdown doesn't work 100% of the time (refresh to fix).<br>
+                - On selecting links in dropdown, text will be changed to that selected link.
             </div>
         </div>
     </div>
