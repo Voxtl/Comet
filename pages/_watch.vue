444923054cf16435eba602758499b039bb3a14a7
LukeHallett <luke@larval.co.uk>
2020-11-08 20:21:16 +0000

    Alter js behaviour

--- pages/_watch.vue
+++ pages/_watch.vue
@@ -54,6 +54,85 @@
                 </div>
             </div>
         </div>
+        <script v-if="ovenPlayerLoaded">
+            player = OvenPlayer.create("player", {
+                sources: [
+                    {
+                        "file": `wss://distribution-1.nue-de.voxtl.com:3334/live/{{this.streamer.user.id}}_source`,
+                        "type": "webrtc",
+                        "label": "Source (Velocity)"
+                    },
+                    {
+                        "file": `https://distribution-1.nue-de.voxtl.com/live/{{this.streamer.user.id}}_source/playlist.m3u8`,
+                        "type": "hls",
+                        "label": "Source (HLS)"
+                    },
+                ],
+                autoStart: true
+            });
+
+            player.on('stateChanged', function(data) {
+                if(data.newstate === 'error') {
+                    console.log(data);
+                }
+            });
+
+            function randomString(length) {
+                let result = '';
+                const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                const charactersLength = characters.length;
+
+                for (let i = 0; i < length; i++) {
+                    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+                }
+
+                return result;
+            }
+
+            socket = new WebSocket('wss://ws.voxtl.tv/{{this.streamer.user.id}}');
+
+            socket.addEventListener('open', function(event) {
+                socket.send(JSON.stringify({
+                    'event': 'join',
+                    'data': '{{this.$auth.loggedIn ? this.$auth.getToken('local').substr(7) : 'guest'}}'
+                }));
+            });
+
+            socket.addEventListener('message', async function(event) {
+                let data = JSON.parse(event.data);
+                const id = randomString(8);
+
+                if(data.event !== 'message') {
+                    return;
+                }
+
+                let userInfo = await axios.get('https://api.voxtl.tv/users/' + data.data.user + '/profile', { headers: { 'Authorization': '{{this.$auth.getToken('local')}}' } });
+                userInfo = userInfo.data.result;
+
+                let badgeMarkup = '';
+
+                // data.badges.forEach(badge => {
+                //     badgeMarkup += '' +
+                //         '<span style="align-self: center;" data-tooltip="' + badge.name + '" data-position="top left" data-variation="mini">' +
+                //             '<i class="small bordered ' + badge.icon + ' icon" style="background-color: #' + badge.color + '; align-self: center; color: #ffffff;">' +
+                //             '</i>' +
+                //         '</span>';
+                // });
+
+                let markup = '' +
+                    '<div class="item" style="display: flex; margin-bottom: 0.2em;">' +
+                        '<img class="ui avatar image" src="https://img.voxtl.tv/user/avatar/' + userInfo.profile.avatar + '.webp" style="align-self: center;">' +
+                        '<div class="content" style="display: flex;">' +
+                            badgeMarkup +
+                            '<div class="header" style="font-size: 1.2em; align-self: center; color: #ffffff; font-weight: 700;">' + userInfo.user.username + '</div>' +
+                            '<div class="description" style="font-size: 1em; align-self: center; margin-left: 0.4em; word-break: break-word; color: #BABABA;" id="' + id + '"></div>' +
+                        '</div>' +
+                    '</div>';
+
+                $('#messages').append(markup);
+                document.getElementById(id).appendChild(document.createTextNode(decodeURI(data.data.message)));
+            });
+        </script>
     </div>
 </template>
 
@@ -61,94 +140,16 @@
     import axios from 'axios';
 
     export default {
-        name: "watch",
+        name: 'watch',
         head() {
             return {
                 title: `${this.streamer.user.username} | Voxtl`,
                 script: [
                     { src: 'https://static.voxtl.tv/script/hls.js' },
+                    { src: 'https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js' },
                     {
                         src: 'https://static.voxtl.tv/player/ovenplayer.js',
                         callback: () => (this.ovenPlayerLoaded = true)
-                    },
-                    {
-                        skip: !this.ovenPlayerLoaded,
-                        innerHTML: `
-                            player = OvenPlayer.create("player", {
-                                sources: [
-                                    {
-                                        "file": 'wss://distribution-1.nue-de.voxtl.com:3334/live/${this.streamer.user.id}_source',
-                                        "type": "webrtc",
-                                        "label": "Source (Velocity)"
-                                    },
-                                    {
-                                        "file": 'https://distribution-1.nue-de.voxtl.com/live/${this.streamer.user.id}_source/playlist.m3u8',
-                                        "type": "hls",
-                                        "label": "Source (HLS)"
-                                    },
-                                ],
-                                autoStart: true
-                            });
-
-                            player.on('stateChanged', function(data) {
-                                if(data.newstate === 'error') {
-
-                                }
-                            });
-                        `,
-                        body: true
-                    },
-                    {
-                        innerHTML: `
-                            function randomString(length) {
-                               let result = '';
-                               const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-                               const charactersLength = characters.length;
-
-                               for (let i = 0; i < length; i++) {
-                                  result += characters.charAt(Math.floor(Math.random() * charactersLength));
-                               }
-
-                               return result;
-                            }
-
-                            socket = new WebSocket('wss://ws.voxtl.tv/${this.streamer.user.id}');
-
-                            socket.addEventListener('open', function(event) {
-                                socket.send(JSON.stringify({
-                                    'event': 'join',
-                                    'data': '${this.$auth.loggedIn ? this.$auth.getToken('local').substr(7) : 'guest'}'
-                                }));
-                            });
-
-                            socket.addEventListener('message', function(event) {
-                                let data = JSON.parse(event.data);
-                                const id = randomString(8);
-
-                                let badgeMarkup = '';
-
-                                data.badges.forEach(badge => {
-                                    badgeMarkup += '' +
-                                        '<span style="align-self: center;" data-tooltip="' + badge.name + '" data-position="top left" data-variation="mini">' +
-                                            '<i class="small bordered ' + badge.icon + ' icon" style="background-color: #' + badge.color + '; align-self: center; color: #ffffff;">' +
-                                            '</i>' +
-                                        '</span>';
-                                });
-
-                                let markup = '' +
-                                    '<div class="item" style="display: flex; margin-bottom: 0.2em;">' +
-                                        '<img class="ui avatar image" src="https://img.voxtl.tv/user/avatar/' + data.viewer.id + '.webp" style="align-self: center;">' +
-                                        '<div class="content" style="display: flex;">' +
-                                            badgeMarkup +
-                                            '<div class="header" style="font-size: 1.2em; align-self: center; color: #ffffff; font-weight: 700;">' + data.viewer.username + '</div>' +
-                                            '<div class="description" style="font-size: 1em; align-self: center; margin-left: 0.4em; word-break: break-word; color: #BABABA;" id="' + id + '"></div>' +
-                                        '</div>' +
-                                    '</div>';
-
-                                $('#messages').append(markup);
-                                document.getElementById(id).appendChild(document.createTextNode(decodeURI(data.message)));
-                            });
-                        `
                     }
                 ]
             }
@@ -157,17 +158,19 @@
             return {
                 ovenPlayerLoaded: false,
                 message: '',
+                intervalID: null
             }
         },
-        asyncData(context) {
-            return axios.get(`https://api.voxtl.tv/users/${context.params.watch}/profile`, { headers: { 'Authorization': `${context.$auth.getToken('local')}` } }).then(res => {
-                return axios.get(`https://api.voxtl.tv/users/${context.params.watch}/channel`, { headers: { 'Authorization': `${context.$auth.getToken('local')}` } }).then(ress => {
-                    return {
-                        streamer: res.data.result,
-                        streamerInfo: ress.data.result
-                    }
-                });
-            });
+        async asyncData(context) {
+            let userProfilePromise = axios.get(`https://api.voxtl.tv/users/${context.params.watch}/profile`, { headers: { 'Authorization': `${context.$auth.getToken('local')}` } });
+            let userChannelPromise = axios.get(`https://api.voxtl.tv/users/${context.params.watch}/channel`, { headers: { 'Authorization': `${context.$auth.getToken('local')}` } });
+
+            let [userProfile, userChannel] = await Promise.all([userProfilePromise, userChannelPromise]);
+
+            return {
+                streamer: userProfile.data.result,
+                streamerInfo: userChannel.data.result
+            }
         },
         methods: {
             async sendMessage() {
@@ -180,14 +183,14 @@
             }
         },
         mounted() {
-            window.setInterval(() => {
-                axios.get(`https://api.voxtl.tv/users/${context.params.watch}/profile`, { headers: { 'Authorization': `${this.$auth.getToken('local')}` } }).then(res => {
-                    axios.get(`https://api.voxtl.tv/users/${context.params.watch}/channel`, { headers: { 'Authorization': `${this.$auth.getToken('local')}` } }).then(ress => {
-                        this.streamer = res.data.result;
-                        this.streamerInfo = ress.data.result;
-                    });
+            this.intervalID = window.setInterval(() => {
+                axios.get(`https://api.voxtl.tv/users/${this.streamer.user.id}/channel`, { headers: { 'Authorization': `${this.$auth.getToken('local')}` } }).then(res => {
+                    this.streamerInfo = res.data.result;
                 });
             }, 5000);
+        },
+        beforeDestroy() {
+            window.clearInterval(this.intervalID);
         }
     }
 </script>
