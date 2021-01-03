fe3005455b46b65c59cb4c89836893b97c7e02a1
Luke Hallett <luke@larval.co.uk>
2021-01-03 14:31:12 +0000

    Update for new api

--- components/blocks/browse/StreamBrowseBlock.vue
+++ components/blocks/browse/StreamBrowseBlock.vue
@@ -1,12 +1,12 @@
 <template>
     <nuxt-link class="ui card category" :to="{ path: stream.user.username }">
         <div class="image">
-            <img :src="'https://img.voxtl.tv/user/thumbnail/' + stream.user.id + '.webp'">
+            <img :src="'https://img.voxtl.tv/user/thumbnail/' + stream.channel.thumbnail + '.webp'">
         </div>
         <div class="content">
             <div class="header">{{ stream.user.username }}</div>
             <p>
-                {{ stream.category.category }}
+                {{ stream.channel.category }}
                 <br>
                 <i class="eye icon"></i> {{ stream.channel.viewers.count }}
             </p>
